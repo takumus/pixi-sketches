769b40800bb6f66ba3200473275be377fb92218c
takumus <me@takumus.com>
2017-11-20 00:01:00 +0900

    tweenテスト。

--- cubic-bezier/src/main.ts
+++ cubic-bezier/src/main.ts
@@ -1,18 +1,35 @@
 import Canvas from '../.src/canvas';
+import CubicBezier from './bezier';
 export default class Main extends Canvas {
+    private t: number;
+    private cbs: ((n: number) => number)[];
     public init() {
+        this.t = 0;
+        this.cbs = [];
+        this.cbs.push(CubicBezier(0.455, 0.03, 0.515, 0.955, 20)); //easeInOutQuad
+        this.cbs.push(CubicBezier(0.77, 0, 0.175, 1, 20)); //easeInOutQuart
+        this.cbs.push(CubicBezier(1, 0, 0, 1)); //easeInOutExpo
+        this.cbs.push(CubicBezier(0.68, -0.55, 0.265, 1.55, 20)); //easeInOutBack
     }
     public draw() {
-    }
-    public mousedown() {
-    }
-    public mouseup() {
-    }
-    public mousemove() {
+        this.t += this.mousePressed ? (this.mouse.x / this.size.width) * 2 : 1;
+        const t = this.t % 60 / 60;
+        const iy = this.size.height * 0.5 / this.cbs.length;
+        const w = this.size.width * 0.5;
+        const bx = this.size.width * 0.25;
+        const by = this.size.height * 0.25 + iy / 2;
         this.canvas.clear();
-        this.canvas.beginFill(0xff0000);
-        this.canvas.drawRect(this.mouse.x - 50, this.mouse.y - 50, 100, 100);
-    }
-    public resize(width: number, height: number) {
+        this.canvas.lineStyle(20, 0xCCCCCC);
+        this.canvas.moveTo(bx, 0);
+        this.canvas.lineTo(bx, this.size.height);
+        this.canvas.moveTo(bx + w, 0);
+        this.canvas.lineTo(bx + w, this.size.height);
+        this.canvas.lineStyle();
+        this.cbs.forEach((cb, id) => {
+            this.canvas.beginFill(0x0000ff);
+            let r = cb(t);
+            if (Math.floor(this.t / 60) % 2 == 0) r = 1 - r;
+            this.canvas.drawRect(bx + r * w - 10, by + id * iy, 20, 20);
+        });
     }
 }
--- cubic-bezier/www/js/bundle.js
+++ cubic-bezier/www/js/bundle.js
@@ -59,27 +59,49 @@
 	    renderer.view.style.height = "100%";
 	    window.addEventListener("resize", resize);
 	    window.addEventListener('orientationchange', resize);
-	    window.addEventListener('mousedown', mousedown);
-	    window.addEventListener('mouseup', mouseup);
-	    window.addEventListener('mousemove', mousemove);
+	    window.addEventListener('mousedown', function (e) {
+	        main.mouse.x = e.clientX;
+	        main.mouse.y = e.clientY;
+	        main.mousePressed = true;
+	        main.mousedown();
+	    });
+	    window.addEventListener('mouseup', function (e) {
+	        main.mousePressed = false;
+	        main.mouseup();
+	    });
+	    window.addEventListener('mousemove', function (e) {
+	        main.mouse.x = e.clientX;
+	        main.mouse.y = e.clientY;
+	        main.mousemove();
+	    });
+	    window.addEventListener('touchstart', function (e) {
+	        main.mouse.x = e.touches[0].clientX;
+	        main.mouse.y = e.touches[0].clientY;
+	        main.mousePressed = true;
+	        main.mousedown();
+	    });
+	    window.addEventListener('touchmove', function (e) {
+	        main.mouse.x = e.touches[0].clientX;
+	        main.mouse.y = e.touches[0].clientY;
+	        main.mousemove();
+	    });
+	    window.addEventListener('touchend', function (e) {
+	        if (e.touches.length > 0)
+	            return;
+	        main.mousePressed = false;
+	        main.mouseup();
+	    });
+	    window.addEventListener('touchcancel', function (e) {
+	        if (e.touches.length > 0)
+	            return;
+	        main.mousePressed = false;
+	        main.mouseup();
+	    });
 	    stage.addChild(main);
 	    draw();
 	    resize();
 	};
 	var ppos = 0;
-	var mousedown = function (e) {
-	    main.mouse.x = e.clientX;
-	    main.mouse.y = e.clientY;
-	    main.mousedown();
-	};
-	var mousemove = function (e) {
-	    main.mouse.x = e.clientX;
-	    main.mouse.y = e.clientY;
-	    main.mousemove();
-	};
-	var mouseup = function (e) {
-	    main.mouseup();
-	};
 	var draw = function () {
 	    requestAnimationFrame(draw);
 	    main.draw();
@@ -110,25 +132,42 @@
 	    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 	};
 	var canvas_1 = __webpack_require__(2);
+	var bezier_1 = __webpack_require__(3);
 	var Main = (function (_super) {
 	    __extends(Main, _super);
 	    function Main() {
 	        return _super !== null && _super.apply(this, arguments) || this;
 	    }
 	    Main.prototype.init = function () {
+	        this.t = 0;
+	        this.cbs = [];
+	        this.cbs.push(bezier_1.default(0.455, 0.03, 0.515, 0.955, 20)); //easeInOutQuad
+	        this.cbs.push(bezier_1.default(0.77, 0, 0.175, 1, 20)); //easeInOutQuart
+	        this.cbs.push(bezier_1.default(1, 0, 0, 1)); //easeInOutExpo
+	        this.cbs.push(bezier_1.default(0.68, -0.55, 0.265, 1.55, 20)); //easeInOutBack
 	    };
 	    Main.prototype.draw = function () {
-	    };
-	    Main.prototype.mousedown = function () {
-	    };
-	    Main.prototype.mouseup = function () {
-	    };
-	    Main.prototype.mousemove = function () {
+	        var _this = this;
+	        this.t += this.mousePressed ? (this.mouse.x / this.size.width) * 2 : 1;
+	        var t = this.t % 60 / 60;
+	        var iy = this.size.height * 0.5 / this.cbs.length;
+	        var w = this.size.width * 0.5;
+	        var bx = this.size.width * 0.25;
+	        var by = this.size.height * 0.25 + iy / 2;
 	        this.canvas.clear();
-	        this.canvas.beginFill(0xff0000);
-	        this.canvas.drawRect(this.mouse.x - 50, this.mouse.y - 50, 100, 100);
-	    };
-	    Main.prototype.resize = function (width, height) {
+	        this.canvas.lineStyle(20, 0xCCCCCC);
+	        this.canvas.moveTo(bx, 0);
+	        this.canvas.lineTo(bx, this.size.height);
+	        this.canvas.moveTo(bx + w, 0);
+	        this.canvas.lineTo(bx + w, this.size.height);
+	        this.canvas.lineStyle();
+	        this.cbs.forEach(function (cb, id) {
+	            _this.canvas.beginFill(0x0000ff);
+	            var r = cb(t);
+	            if (Math.floor(_this.t / 60) % 2 == 0)
+	                r = 1 - r;
+	            _this.canvas.drawRect(bx + r * w - 10, by + id * iy, 20, 20);
+	        });
 	    };
 	    return Main;
 	}(canvas_1.default));
@@ -174,6 +213,67 @@
 	}(PIXI.Container));
 	Object.defineProperty(exports, "__esModule", { value: true });
 	exports.default = Canvas;
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	"use strict";
+	function CubicBezier(p1x, p1y, p2x, p2y, res) {
+	    if (res === void 0) { res = 20; }
+	    var xtList = [];
+	    p1y = 1 - p1y;
+	    p2y = 1 - p2y;
+	    for (var i = 0; i <= res; i++) {
+	        var t = i / res;
+	        xtList.push({
+	            x: bezier(t, 0, p1x, p2x, 1),
+	            t: t
+	        });
+	    }
+	    return function (x) {
+	        x = x < 0 ? 0 : x > 1 ? 1 : x;
+	        var i = search(xtList, res, x);
+	        var a = xtList[i];
+	        var b = xtList[i + 1];
+	        return 1 - bezier(
+	        //tを計算
+	        (x - a.x) / (b.x - a.x) * (b.t - a.t) + a.t, 
+	        //yを求める
+	        1, p1y, p2y, 0);
+	    };
+	}
+	Object.defineProperty(exports, "__esModule", { value: true });
+	exports.default = CubicBezier;
+	//2分探索もどきで探索
+	function search(xtList, length, x) {
+	    if (x == 0)
+	        return 0;
+	    if (x == 1)
+	        return length - 2;
+	    var low = 0;
+	    var high = length - 1;
+	    while (low <= high) {
+	        var i = Math.floor((low + high) / 2);
+	        if (xtList[i].x == x) {
+	            return i;
+	        }
+	        else if (xtList[i].x > x) {
+	            high = i - 1;
+	        }
+	        else {
+	            low = i + 1;
+	        }
+	    }
+	    return low - 1;
+	}
+	;
+	// maximaで求めたらこうなったベジェ関数。
+	function bezier(t, p0, p1, p2, p3) {
+	    var mt = 1 - t;
+	    return p3 * t * t * t + 3 * mt * p2 * t * t + 3 * mt * mt * p1 * t + mt * mt * mt * p0;
+	}
 
 
 /***/ }
