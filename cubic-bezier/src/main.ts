3630baeed7e73f2d62ef491f0d06a572ed4044d3
takumus <me@takumus.com>
2017-11-20 00:01:42 +0900

    不要削除

--- cubic-bezier/src/main.ts
+++ cubic-bezier/src/main.ts
@@ -6,10 +6,10 @@ export default class Main extends Canvas {
     public init() {
         this.t = 0;
         this.cbs = [];
-        this.cbs.push(CubicBezier(0.455, 0.03, 0.515, 0.955, 20)); //easeInOutQuad
-        this.cbs.push(CubicBezier(0.77, 0, 0.175, 1, 20)); //easeInOutQuart
+        this.cbs.push(CubicBezier(0.455, 0.03, 0.515, 0.955)); //easeInOutQuad
+        this.cbs.push(CubicBezier(0.77, 0, 0.175, 1)); //easeInOutQuart
         this.cbs.push(CubicBezier(1, 0, 0, 1)); //easeInOutExpo
-        this.cbs.push(CubicBezier(0.68, -0.55, 0.265, 1.55, 20)); //easeInOutBack
+        this.cbs.push(CubicBezier(0.68, -0.55, 0.265, 1.55)); //easeInOutBack
     }
     public draw() {
         this.t += this.mousePressed ? (this.mouse.x / this.size.width) * 2 : 1;
--- cubic-bezier/www/js/bundle.js
+++ cubic-bezier/www/js/bundle.js
@@ -141,10 +141,10 @@
 	    Main.prototype.init = function () {
 	        this.t = 0;
 	        this.cbs = [];
-	        this.cbs.push(bezier_1.default(0.455, 0.03, 0.515, 0.955, 20)); //easeInOutQuad
-	        this.cbs.push(bezier_1.default(0.77, 0, 0.175, 1, 20)); //easeInOutQuart
+	        this.cbs.push(bezier_1.default(0.455, 0.03, 0.515, 0.955)); //easeInOutQuad
+	        this.cbs.push(bezier_1.default(0.77, 0, 0.175, 1)); //easeInOutQuart
 	        this.cbs.push(bezier_1.default(1, 0, 0, 1)); //easeInOutExpo
-	        this.cbs.push(bezier_1.default(0.68, -0.55, 0.265, 1.55, 20)); //easeInOutBack
+	        this.cbs.push(bezier_1.default(0.68, -0.55, 0.265, 1.55)); //easeInOutBack
 	    };
 	    Main.prototype.draw = function () {
 	        var _this = this;
