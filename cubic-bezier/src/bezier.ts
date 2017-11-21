1b8b54421982d0603b7124dec96b1b5441742277
takumus <me@takumus.com>
2017-11-22 02:16:40 +0900

    調整

--- cubic-bezier/src/bezier.ts
+++ cubic-bezier/src/bezier.ts
@@ -1,3 +1,4 @@
+type XT = {x: number, t: number};
 export default function CubicBezier(
     p1x: number,
     p1y: number,
@@ -5,47 +6,45 @@ export default function CubicBezier(
     p2y: number,
     res: number = 20
 ){
-    const xtList: {x: number, t: number}[] = [];
+    const xList: number[] = [];
+    const tList: number[] = [];
     p1y = 1 - p1y;
     p2y = 1 - p2y;
     for (let i = 0; i <= res; i ++) {
         const t = i / res;
-        xtList.push({
-            x: bezier(t, 0, p1x, p2x, 1),
-            t: t
-        });
+        xList.push(bezier(t, 0, p1x, p2x, 1));
+        tList.push(t);
     }
     return function (x: number): number {
         x = x < 0 ? 0 : x > 1 ? 1 : x;
-        const i = search(xtList, res, x);
-        const a = xtList[i];
-        const b = xtList[i + 1];
+        const i = search(xList, res, x);
+        const ax = xList[i];
+        const bx = xList[i + 1];
+        const at = tList[i];
+        const bt = tList[i + 1];
         return 1 - bezier(
             //tを計算
-            (x - a.x) / (b.x - a.x) * (b.t - a.t) + a.t,
+            (x - ax) / (bx - ax) * (bt - at) + at,
             //yを求める
             1, p1y, p2y, 0
         );
     }
 }
 //2分探索もどきで探索
-function search(xtList: {x: number, t: number}[], length: number, x: number) {
-    if (x == 0) return 0;
-    if (x == 1) return length - 2;
-    let low = 0;
-    let high = length - 1;
-    while(low <= high){
-        var i = Math.floor((low + high) / 2);
-        if(xtList[i].x == x) {
-            return i;
-        }else if(xtList[i].x > x) {
-            high = i - 1;
-        }else {
-            low = i + 1;
-        }
+function search(arr: number[], len: number, n: number) {
+    if (n == 0) return 0;
+    let l = 0;
+    let r = len - 1;
+    while(l <= r) {
+       const m = ~~((l + r) * 0.5);
+       if (arr[m] < n) {
+          l = m + 1;
+       }else {
+          r = m - 1;
+       }
     }
-    return low - 1;
-};
+    return r;
+ }
 // maximaで求めたらこうなったベジェ関数。
 function bezier(t: number, p0: number, p1: number, p2: number, p3: number) {
     const mt = 1 - t;
--- cubic-bezier/www/js/bundle.js
+++ cubic-bezier/www/js/bundle.js
@@ -222,24 +222,25 @@
 	"use strict";
 	function CubicBezier(p1x, p1y, p2x, p2y, res) {
 	    if (res === void 0) { res = 20; }
-	    var xtList = [];
+	    var xList = [];
+	    var tList = [];
 	    p1y = 1 - p1y;
 	    p2y = 1 - p2y;
 	    for (var i = 0; i <= res; i++) {
 	        var t = i / res;
-	        xtList.push({
-	            x: bezier(t, 0, p1x, p2x, 1),
-	            t: t
-	        });
+	        xList.push(bezier(t, 0, p1x, p2x, 1));
+	        tList.push(t);
 	    }
 	    return function (x) {
 	        x = x < 0 ? 0 : x > 1 ? 1 : x;
-	        var i = search(xtList, res, x);
-	        var a = xtList[i];
-	        var b = xtList[i + 1];
+	        var i = search(xList, res, x);
+	        var ax = xList[i];
+	        var bx = xList[i + 1];
+	        var at = tList[i];
+	        var bt = tList[i + 1];
 	        return 1 - bezier(
 	        //tを計算
-	        (x - a.x) / (b.x - a.x) * (b.t - a.t) + a.t, 
+	        (x - ax) / (bx - ax) * (bt - at) + at, 
 	        //yを求める
 	        1, p1y, p2y, 0);
 	    };
@@ -247,28 +248,22 @@
 	Object.defineProperty(exports, "__esModule", { value: true });
 	exports.default = CubicBezier;
 	//2分探索もどきで探索
-	function search(xtList, length, x) {
-	    if (x == 0)
+	function search(arr, len, n) {
+	    if (n == 0)
 	        return 0;
-	    if (x == 1)
-	        return length - 2;
-	    var low = 0;
-	    var high = length - 1;
-	    while (low <= high) {
-	        var i = Math.floor((low + high) / 2);
-	        if (xtList[i].x == x) {
-	            return i;
-	        }
-	        else if (xtList[i].x > x) {
-	            high = i - 1;
+	    var l = 0;
+	    var r = len - 1;
+	    while (l <= r) {
+	        var m = ~~((l + r) * 0.5);
+	        if (arr[m] < n) {
+	            l = m + 1;
 	        }
 	        else {
-	            low = i + 1;
+	            r = m - 1;
 	        }
 	    }
-	    return low - 1;
+	    return r;
 	}
-	;
 	// maximaで求めたらこうなったベジェ関数。
 	function bezier(t, p0, p1, p2, p3) {
 	    var mt = 1 - t;
